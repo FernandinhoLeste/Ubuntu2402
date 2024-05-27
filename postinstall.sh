f4b4b6023d6fa2dc9989919fcdce805767979e95
FernandinhoLeste <109706684+FernandinhoLeste@users.noreply.github.com>
2024-05-27 15:41:39 -0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-curl --tlsv1.2 --silent --show-error --header 'x-connect-key: ca54fc037d798fb2e1103c7cb8a486ac4941bb64' https://kickstart.jumpcloud.com/Kickstart | sudo bash
 
+sudo apt-get update
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+sudo apt-get install -f -y
+rm /tmp/google-chrome-stable_current_amd64.deb
+
+if command -v google-chrome > /dev/null; then
+    echo "Google Chrome instalado com sucesso!"
+else
+    echo "Houve um problema na instalação do Google Chrome."
+fi
 
 
