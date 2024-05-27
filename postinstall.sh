3443f56bd90f0a00ea6a395d43e8db0753fc0c22
FernandinhoLeste <109706684+FernandinhoLeste@users.noreply.github.com>
2024-05-27 17:27:41 -0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -15,4 +15,11 @@ fi
 #Instalação JumpCloud
 curl --tlsv1.2 --silent --show-error --header 'x-connect-key: ca54fc037d798fb2e1103c7cb8a486ac4941bb64' https://kickstart.jumpcloud.com/Kickstart | sudo bash
 
+#Instalação Wazuh
+wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.6.0-1_amd64.deb && sudo WAZUH_MANAGER='starwatch.benomad-sec.internal' WAZUH_AGENT_GROUP='LNX_EndUsers' dpkg -i ./wazuh-agent_4.6.0-1_amd64.deb
+
+sudo systemctl daemon-reload
+sudo systemctl enable wazuh-agent
+sudo systemctl start wazuh-agent
+
 
