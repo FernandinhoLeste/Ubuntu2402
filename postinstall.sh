b0288cca626bb6863e2ee2939f0ad1ae87f44903
FernandinhoLeste <109706684+FernandinhoLeste@users.noreply.github.com>
2024-05-27 14:11:28 -0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -29,4 +29,10 @@ else
 	# Exibe processo do agente em execucao
 	ps -ax -o user:96,pid,cmd | grep "[s]tAgentApp"
 fi
-exit
+
+
+
+curl --tlsv1.2 --silent --show-error --header 'x-connect-key: ca54fc037d798fb2e1103c7cb8a486ac4941bb64' https://kickstart.jumpcloud.com/Kickstart | sudo bash
+
+
+
